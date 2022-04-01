0c163d3bd0528036edcc887e316d80608ab451fd
Shubh03 <63538469+Shubh03@users.noreply.github.com>
2022-03-31 22:57:44 -0400

    Add files via upload

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("127.0.0.", "root", "", "carrental"); 
+$con=mysqli_connect("us-cdbr-east-05.cleardb.net", "bc343e20b4cb9a", "3f3e26cb", "heroku_eeb1b643f6cf021"); 
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }

f0d174f360f497b20b4e566ac728ee1c1a7f57f0
Shubh03 <63538469+Shubh03@users.noreply.github.com>
2022-04-09 22:53:09 -0400

    Add files via upload

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -29,8 +29,10 @@
 		
 
 				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li>
 				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
-
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
 
 			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
 

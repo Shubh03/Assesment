4403cd00578ff9bc438301530a0bf37da35ce806
Shubh03 <63538469+Shubh03@users.noreply.github.com>
2022-04-09 22:26:19 -0400

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -19,17 +19,7 @@
         </span> -->
         <div class="header_info">
           
-<?php
-$sql = "SELECT EmailId,ContactNo from tblcontactusinfo;";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':vhid',$vhid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-foreach ($results as $result) {
-$email=$result->EmailId;
-$contactno=$result->ContactNo;
-}
-?>    
+
 
             <!-- <div class="header_widgets">
               <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
@@ -43,7 +33,7 @@ $contactno=$result->ContactNo;
             <div class="social-follow">
             
             </div> -->
-<?php   if(strlen($_SESSION['login'])==0)
+   <?php   if(strlen($_SESSION['login'])==0)
 	{	
 ?>
  <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
@@ -72,7 +62,7 @@ echo "Welcome To Car rental portal";
       <?php if($_SESSION['login']){?>
       <ul>
             <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i  aria-hidden="true"></i> 
-<!-- <?php 
+<?php 
 $email=$_SESSION['login'];
 $sql ="SELECT * FROM tblusers WHERE EmailId=:email ";
 $query= $dbh -> prepare($sql);
@@ -87,7 +77,7 @@ foreach($results as $result)
     
    
   
-   }}?> -->
+   }}?>
    
              <i class="fa fa-angle-down" aria-hidden="true"></i></a>
             <img src="uploads/<?php echo htmlentities($result->Vimage1);?>" class="header-user"width="30" height="30" >

7bb8762fed53fd6feee82e1f844a3619b1e58b9c
Shubh03 <63538469+Shubh03@users.noreply.github.com>
2022-04-11 21:06:39 -0400

    Delete includes directory

--- includes/dbconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=mysqli_connect("us-cdbr-east-05.cleardb.net", "bc343e20b4cb9a", "3f3e26cb", "heroku_eeb1b643f6cf021"); 
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
-}
-
-  ?>
